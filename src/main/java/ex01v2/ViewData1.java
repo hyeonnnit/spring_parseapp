b92e7de1311ef4914061d413251fa7c334c17d42
hyeon <lhyeonnit@gmail.com>
2024-02-16 09:48:57 +0900

    ex01v2, v3

--- src/main/java/ex01v2/ViewData1.java
+++ src/main/java/ex01v2/ViewData1.java
@@ -1,17 +1,22 @@
 package ex01v2;
 
-import ex01.model.User;
+import ex01v2.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Data
 public class ViewData1 {
     private int boardId;
     private String title;
     private String content;
-
     private User user;
+
+    public ViewData1(DBData1 dbData) {
+        this.boardId = dbData.getBoardId();
+        this.title = dbData.getTitle();
+        this.content = dbData.getContent();
+        this.user = new User();
+    }
 }
